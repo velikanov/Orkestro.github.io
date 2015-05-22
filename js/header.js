ecab71b36ff43d48ef63335dfed3010936e5ad63
velikan (home) <velikan55555@hotmail.com>
2015-05-23 01:51:13 +0300

    update

--- js/header.js
+++ js/header.js
@@ -6,5 +6,6 @@
 
     $('header .header-content .header-content-inner h1').html($('header input:hidden[name^="header-"]:eq(' + firstTitleIndex + ')').val());
     $('header .header-content .header-content-inner p.description').html($('header input:hidden[name^="description-"]:eq(' + firstTitleIndex + ')').val());
-    $('header .header-content .header-content-inner a').html($('header input:hidden[name^="button-"]:eq(' + firstTitleIndex + ')').val());
+    $('header .header-content .header-content-inner a').html($('header input:hidden[name^="button_title-"]:eq(' + firstTitleIndex + ')').val());
+    $('header .header-content .header-content-inner a').attr('href', $('header input:hidden[name^="button_url-"]:eq(' + firstTitleIndex + ')').val());
 })(jQuery);;
